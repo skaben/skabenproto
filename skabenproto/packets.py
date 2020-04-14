0c67cbb2db15d9fb28623b33cf84c3553162dcdf
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-04-14 09:42:51 +0000

    packet simplified

--- skabenproto/packets.py
+++ skabenproto/packets.py
@@ -3,22 +3,18 @@ class BasePacket:
         Base packet class
     """
 
-    def __init__(self, dev_type, uid=None):
-        self.payload = dict()  # data payload
-        self.command = str()  # command, assign in child classes
-        self.dev_type = dev_type  # group channel address
+    command = str()
 
-        # WARNING: uid here will be used for addressing.
-        self.uid = uid  # personal channel address, optional
-        self.ts = None  # timestamp will be assigned by encoder
+    def __init__(self, dev_type, uid=None, timestamp=None):
+        self.topic = self.dev_type if not uid else f"{dev_type}/{uid}"  # assign mqtt topic
+        self.timestamp = timestamp if timestamp else 0  # assign timestamp if provided
+        self.content = {
+            'command': self.command,
+            'timestamp': self.timestamp,
+        }
 
     def __repr__(self):
-        if self.uid:
-            ident = '{}/{}'.format(self.dev_type, self.uid)
-        else:
-            ident = self.dev_type
-
-        return '<{}> {}'.format(self.command, ident)
+        return f"{self.topic} {self.data}"
 
 # ping packets
 
@@ -27,10 +23,11 @@ class PING(BasePacket):
     """
         Ping packet. Broadcast only.
     """
-    def __init__(self, dev_type, uid=None):
-        super().__init__(dev_type=dev_type,
-                         uid=uid)
+    def __init__(self, dev_type, uid=None, timestamp=None):
         self.command = 'PING'
+        super().__init__(dev_type=dev_type,
+                         uid=uid,
+                         timestamp=timestamp)
 
 
 class PONG(BasePacket):
@@ -38,11 +35,11 @@ class PONG(BasePacket):
         PONG packet, send only in response to PING
         Should send timestamp value of PING
     """
-    def __init__(self, dev_type, uid):
+    def __init__(self, dev_type, uid, timestamp):
+        self.command = 'PONG'
         super().__init__(dev_type=dev_type,
                          uid=uid,
-                         )
-        self.command = 'PONG'
+                         timestamp=timestamp)
 
 
 class PINGLegacy(BasePacket):
@@ -50,9 +47,8 @@ class PINGLegacy(BasePacket):
         Ping packet. Broadcast only.
     """
     def __init__(self, dev_type):
-        super().__init__(dev_type=dev_type,
-                         )
         self.command = '*/PING'
+        super().__init__(dev_type=dev_type)
 
 # service packets
 
@@ -63,18 +59,18 @@ class WAIT(BasePacket):
         before sending another PONG client should either
         wait timeout or receive nowait-packet (CUP/SUP)
     """
-    def __init__(self, dev_type, timeout, uid):
+    def __init__(self, dev_type, timeout, uid, timestamp):
+        self.command = 'WAIT'
         super().__init__(dev_type=dev_type,
                          uid=uid,
-                         )
+                         timestamp=timestamp)
 
         if not isinstance(timeout, int):
             try:
                 timeout = round(int(timeout))
             except Exception:
                 raise ValueError('bad timeout value: %s' % timeout)
-        self.command = 'WAIT'
-        self.payload.update({'timeout': timeout})
+        self.content.update({'timeout': timeout})
 
 
 class ACK(BasePacket):
@@ -82,12 +78,12 @@ class ACK(BasePacket):
         Confirm operations on previous packet as successful
         Should send ts of previous packet
     """
-    def __init__(self, dev_type, task_id, uid):
+    def __init__(self, dev_type, task_id, uid, timestamp):
+        self.command = 'ACK'
         super().__init__(dev_type=dev_type,
                          uid=uid,
-                         )
-        self.command = 'ACK'
-        self.payload.update({'task_id': task_id})
+                         timestamp=timestamp)
+        self.content.update({'task_id': task_id})
 
 
 class NACK(BasePacket):
@@ -95,12 +91,12 @@ class NACK(BasePacket):
         Confirm operations on previous packet as unsuccessful
         Should send ts of previous packet
     """
-    def __init__(self, dev_type, task_id, uid):
+    def __init__(self, dev_type, task_id, uid, timestamp):
+        self.command = 'NACK'
         super().__init__(dev_type=dev_type,
                          uid=uid,
-                         )
-        self.command = 'NACK'
-        self.payload.update({'task_id': task_id})
+                         timestamp=timestamp)
+        self.content.update({'task_id': task_id})
 
 
 # packets with payload
@@ -113,58 +109,45 @@ class PayloadPacket(BasePacket):
 
     payload = {}  # packet inner payload
 
-    def __init__(self, dev_type, payload,  uid=None, task_id=None):
+    def __init__(self, dev_type, payload, timestamp, uid=None, task_id=None):
         super().__init__(dev_type=dev_type,
                          uid=uid,
-                         )
-
-        if task_id:
-            self.payload.update({'task_id': task_id})
-        if not isinstance(payload, dict):
-            raise Exception(f'payload should be a dictionary, not a {type(payload)}')
-        if not dev_type == 'dumb':
-            self.payload.update(**payload)
-        else:
-            self.payload = payload
+                         timestamp=timestamp)
+        self.content.update({'payload': payload})  # separated namespace
 
 
 class INFO(PayloadPacket):
     """
         Multipurpose payload packet
     """
-    def __init__(self, dev_type, payload, uid, task_id=None):
+    def __init__(self, dev_type, payload, uid, timestamp, task_id=None):
+        self.command = 'INFO'
         super().__init__(dev_type=dev_type,
                          payload=payload,
+                         timestamp=timestamp,
                          task_id=task_id,
-                         uid=uid,
-                         )
-
-        self.command = 'INFO'
-
+                         uid=uid)
 
 class CUP(PayloadPacket):
     """
         Client UPdate - update client config
     """
-    def __init__(self, dev_type, payload, task_id,  uid=None):
+    def __init__(self, dev_type, payload, task_id, timestamp, uid=None):
+        self.command = 'CUP'
         super().__init__(dev_type=dev_type,
                          payload=payload,
                          task_id=task_id,
                          uid=uid,
-                         )
-
-        self.command = 'CUP'
-
+                         timestamp=timestamp)
 
 class SUP(PayloadPacket):
     """
         Server UPdate - update server config
     """
-    def __init__(self, dev_type, payload, uid, task_id=None):
+    def __init__(self, dev_type, payload, uid, timestamp, task_id=None):
+        self.command = 'SUP'
         super().__init__(dev_type=dev_type,
                          payload=payload,
                          task_id=task_id,
                          uid=uid,
-                         )
-
-        self.command = 'SUP'
+                         timestamp=timestamp)
