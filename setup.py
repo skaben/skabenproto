4cf554721bfaa98bce8b593b951ac4b397e51bd4
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-07-25 13:12:00 +0300

    versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='skabenproto',
-   version='1.19',
+   version='2.00',
    description='SKABEN protocol',
    license="MIT",
    long_description=long_description,
